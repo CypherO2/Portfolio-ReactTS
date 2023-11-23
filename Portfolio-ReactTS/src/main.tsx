bc3530f55782599ce05ddbb3da85b08fb45ae90c
Cassi <120105652+CypherO2@users.noreply.github.com>
2023-11-23 13:55:51 +0000

    dsf

--- Portfolio-ReactTS/src/main.tsx
+++ Portfolio-ReactTS/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "../node_modules/react-bootstrap/dist/react-bootstrap.min.js";
+// import "../node_modules/react-bootstrap/dist/react-bootstrap.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
