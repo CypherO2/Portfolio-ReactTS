6febbd18d64ef07978c245b7bf2142608ef6fdfc
Cassi <120105652+CypherO2@users.noreply.github.com>
2023-11-23 13:54:57 +0000

    sdf

--- Portfolio-ReactTS/src/main.tsx
+++ Portfolio-ReactTS/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "../node_modules/react-bootstrap/dist/react-bootstrap.min.js";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
